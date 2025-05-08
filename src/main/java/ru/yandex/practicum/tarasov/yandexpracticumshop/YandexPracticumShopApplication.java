55abfb287573642b8cd4f885f880943d15894329
Andrey Tarasov <andvatar@gmail.com>
2025-05-08 19:04:11 +0300

    db

---
+++ src/main/java/ru/yandex/practicum/tarasov/yandexpracticumshop/YandexPracticumShopApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.tarasov.yandexpracticumshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YandexPracticumShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YandexPracticumShopApplication.class, args);
+    }
+
+}
